a1bddb185e91ccfcdf6cace4a10535cdcc0293ca
Zahid Mahir <zahid@zmhr.me>
2016-09-08 19:56:28 -0400

    Simplified process and cleaned up output

--- db/backup.sh
+++ db/backup.sh
@@ -7,25 +7,15 @@ echo "Making a backup of mongodb"
 
 # make a temporary copy of the database dump
 if [ -d "/db/dump/" ]; then
-    echo "making a temporary copy of /db/dump at /db/dump/"
-    mv /db/dump/ /db/.tmp/
+    # echo "Making a temporary copy of /db/dump at /db/dump/"
+    mv /db/dump/ /db/temp/
 fi
 
-# mongodump --host db --out /db/dump/
-if ! mongodump --host db --out /db/dump/ > backup-error.log 2>&1 ; then
-    echo "mongodb backup failed"
+mongodump --host db --out /db/dump/
 
-    # put back old copy of the backup
-    rm -rf /db/dump/
-    mv .tmp/ /db/dump/
-
-    cat backup-error.log
-    exit 1
+if [ -d "/db/temp/" ]; then
+    # echo "Deleting temporary copy of database"
+    rm -rf /db/temp/
 fi
 
-echo "I think it worked?"
-
-if [ -d "/db/.tmp/" ]; then
-    echo "deleting temporary copy of database"
-    rm -rf /db/.tmp/
-fi
+echo "Done"
