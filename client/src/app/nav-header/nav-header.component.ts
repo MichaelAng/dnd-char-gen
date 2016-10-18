2f5e3a818b17d2ca51fdca7df4a4f90e3e13e719
Michael Ang <angmerica@gmail.com>
2016-10-18 17:57:13 -0400

    add a basic nav-header html

---
+++ client/src/app/nav-header/nav-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'nav-header',
+    templateUrl: './nav-header.component.html',
+    styleUrls: ['./nav-header.component.css']
+})
+export class NavHeaderComponent {
+    title = 'Hello World';
+}
