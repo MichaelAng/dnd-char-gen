ebc049c9b366cbf4ffbb208ac0b277b65eb2c315
Michael Ang <angmerica@gmail.com>
2016-10-19 20:33:21 -0400

    created container and iterate the list of spells

--- client/src/app/spell-list/spell-list.component.ts
+++ client/src/app/spell-list/spell-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { spells } from '../../../public/data/spells';
 
 @Component({
     selector: 'spell-list',
     templateUrl: './spell-list.component.html',
     styleUrls: ['./spell-list.component.css']
 })
-export class SpellListComponent {
+export class SpellListComponent implements OnInit {
     title = 'Hello World';
+    spells: any;
+
+    ngOnInit () {
+        this.spells = spells;
+    }
 }
