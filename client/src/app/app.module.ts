d688c4c26d458c78f835fea145cc0ca0f1a1ba4c
Michael Ang <angmerica@gmail.com>
2016-10-25 16:07:12 -0400

    renamed files for consistency

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule }        from '@angular/platform-browser';
 import { NgModule }             from '@angular/core';
 
 import { AppComponent }         from './app.component';
-import { AppRoutingModule }     from './app.routing';
+import { AppRoutingModule }     from './app-routing.module';
 
 // Core Modules
 import { CoreModule }      from './core/core.module';
