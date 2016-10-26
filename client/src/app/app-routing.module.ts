233ed12a50b964856bc6860ce4a7fd08ddc594b2
Michael Ang <angmerica@gmail.com>
2016-10-26 17:20:31 -0400

    add spells route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule }     from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SpellListComponent } from './spell-list/spell-list.component';
+
 const routes: Routes = [
+    { path: 'spells', component: SpellListComponent },
     { path: '', redirectTo: 'spells', pathMatch: 'full' },
     { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
     imports: [
-      RouterModule.forRoot(routes)
+        RouterModule.forRoot(routes)
     ],
     exports: [
-      RouterModule
+        RouterModule
     ]
 })
 export class AppRoutingModule {}
