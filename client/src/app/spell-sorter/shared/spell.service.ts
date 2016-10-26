5374928a9341de2ffb4de56ac088190dd3c25219
Michael Ang <angmerica@gmail.com>
2016-10-26 18:50:49 -0400

    remove comment

--- client/src/app/spell-sorter/shared/spell.service.ts
+++ client/src/app/spell-sorter/shared/spell.service.ts
@@ -24,7 +24,6 @@ export class SpellService {
     }
 
     private handleError (error: Response | any) {
-        // In a real world app, we might use a remote logging infrastructure
         let errMsg: string;
         if (error instanceof Response) {
             const body = error.json() || '';
