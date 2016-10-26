750d7ad82bff4c2d1d1a7b80c51b38d194567d0b
Michael Ang <angmerica@gmail.com>
2016-10-26 16:56:21 -0400

    suppress the element error

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 import { AppComponent } from './app.component';
 
 describe('App', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
-  });
-  it ('should work', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent],
+            schemas: [ NO_ERRORS_SCHEMA ]
+        });
+    });
+    it ('should work', () => {
+        let fixture = TestBed.createComponent(AppComponent);
+        expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+    });
 });
